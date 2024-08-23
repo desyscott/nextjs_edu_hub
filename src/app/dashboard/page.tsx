2ef9c56af6c922a5b60f0806c6a567dbc2f35e4c
desy scott <Mrfallback055@gmail.com>
2024-08-23 21:37:11 +0000

    configured font family

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Dashboard() {
+  return (
+    <main className="font-gilroy">
+  
+      <h1 className="text-4xl">Dashboard</h1>
+      <p className="font-sora">Get started today!</p>
+     
+  
+    </main>
+  );
+}
